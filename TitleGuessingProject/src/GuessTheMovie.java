92b65ba6ad9e882afd7e56852adc6b19f64f50dd
libonancaesar <libonancaesar@yahoo.com>
2022-10-21 10:37:42 -0500

    create guessing project

---
+++ TitleGuessingProject/src/GuessTheMovie.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+
+public class GuessTheMovie {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        File file = new File("movies.txt");
+
+    }
+}
